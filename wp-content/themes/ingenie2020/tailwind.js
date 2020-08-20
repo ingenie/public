d0680d4992ed587d0b0007c7122222b083f5d046
Elton <elton@ingenie.com>
2020-08-20 15:55:41 +0100

    chore: organise gitignore and shippable

---
+++ wp-content/themes/ingenie2020/tailwind.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // error from tailwind abut purge without below TODO
+  // purge: [
+  //   "*.php",
+  //   "templates/**/*.php",
+  //   "build/js/**/*.js",
+  // ],
+  theme: {
+    container: {
+      padding: '1.5rem',
+    },
+    extend: {
+      colors: {
+        link: {
+          'default': '#3182ce',
+          'hover': '#63b3ed',
+        }
+      },
+      fontSize: {
+        xxs: '0.675rem',
+      },
+      lineHeight: {
+        tighter: '1.125',
+      },
+    }
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'visited'],
+  },
+  plugins: [
+    ({ addUtilities }) => {
+      const utils = {
+        '.translate-x-half': {
+          transform: 'translateX(50%)',
+        },
+      };
+      addUtilities(utils, ['responsive'])
+    }
+  ]
+};
