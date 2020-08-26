a572e8991d36266dad99d1bbb5512ab2252659c5
Elton <elton@ingenie.com>
2020-08-26 10:48:02 +0100

    chore: alt YDG header

---
+++ wp-content/themes/ingenie2020/header-ydg.php
@@ -0,0 +1,5 @@
+<?php get_template_part( 'templates/partials/document-open' ); ?>
+
+<?php get_template_part( 'templates/partials/navbar', 'main' ); ?>
+YOUNG DRIVERS GUIDE HEADER!!!
+<!-- Site header markup goes here -->
