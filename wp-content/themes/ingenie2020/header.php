8952b1adea2d89922184d0830e1cec9c5c5daf62
Elton <elton@ingenie.com>
2020-08-26 10:40:58 +0100

    chore: added some ig colors tw

--- wp-content/themes/ingenie2020/header.php
+++ wp-content/themes/ingenie2020/header.php
@@ -1,3 +1,6 @@
 <?php get_template_part( 'templates/partials/document-open' ); ?>
 
+<?php get_template_part( 'templates/partials/navbar', 'main' ); ?>
+STANDARD HEADER!!!
+
 <!-- Site header markup goes here -->
