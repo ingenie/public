5eec0172e1ff181d521cc09a0030d2e7912ac711
Elton <elton@ingenie.com>
2021-05-05 10:50:46 +0100

    chore: plugin updates

---
+++ wp-content/plugins/ingenie-stats/ingenie-stats.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: ingenie Business Stats
+Plugin URI: http://ingenie.com
+Description: ingenie Business stats to change / keep all stats in check across environments. Can be used in all pages, posts, and templates.
+Version: 1.0
+Author: ingenie
+Author URI: http://ingenie.com
+*/
+
+/*
+// ------------------------
+// USAGE
+
+For use in landing pages/templates etc:
+<?php echo do_shortcode("[learner_get_a_quote_url]"); ?>
+
+For use in WYSIWYG Editor:
+[learner_get_a_quote_url]
+
+// ------------------------
+*/
+
+// ------------------------
+//
+// LEARNER DRIVER STATS
+//
+// ------------------------
+
+if ( ! defined('ABSPATH')) {
+echo 'What you after the stats for, dummy!';
+die;
+}
+
+function learner_get_a_quote_url($atts) {
+$learner_get_a_quote_url = "//learner.ingenie.com/?Brand=DEFAULT&referrer=ingenie";
+return $learner_get_a_quote_url;
+}
+function learner_quotes_start_from_txt($atts) {
+$learner_quotes_start_from_txt = "£1.49";
+return $learner_quotes_start_from_txt;
+}
+function learner_low_excess_txt($atts) {
+$learner_low_excess_txt = "£250";
+return $learner_low_excess_txt;
+}
+function learner_sp_dates_discount_txt($atts) {
+$learner_sp_dates_discount_txt = "Average daily price based on all policies sold between July 2020 - Oct 2020.";
+return $learner_sp_dates_discount_txt;
+}
+// ------------------------
+//
+// YOUNG DRIVER STATS
+//
+// ------------------------
+function yd_get_a_quote_url($atts) {
+$yd_get_a_quote_url = "//myquote.ingenie.com/?Brand=DEFAULT&referrer=ingenie";
+return $yd_get_a_quote_url;
+}
+function yd_up_front_txt($atts) {
+$yd_up_front_txt = "£400";
+return $yd_up_front_txt;
+}
+function yd_drive_safe_discount_txt($atts) {
+$yd_drive_safe_discount_txt = "21%";
+return $yd_drive_safe_discount_txt;
+}
+function yd_renewal_discount_txt($atts) {
+$yd_renewal_discount_txt = "£563";
+return $yd_renewal_discount_txt;
+}
+function yd_renewal_percentage_txt($atts) {
+$yd_renewal_percentage_txt = "37%";
+return $yd_renewal_percentage_txt;
+}
+function yd_frac_cus_discount_txt($atts) {
+$yd_frac_cus_discount_txt = "7";
+return $yd_frac_cus_discount_txt;
+}
+function yd_sp_dates_discount_txt($atts) {
+$yd_sp_dates_discount_txt = "July 2017 to June 2018";
+return $yd_sp_dates_discount_txt;
+}
+function yd_sp_dates_discount_txt_2($atts) {
+$yd_sp_dates_discount_txt_2 = "Based on all ingenie customers between July 2018 to June 2019.";
+return $yd_sp_dates_discount_txt_2;
+}
+function yd_sp_premium_increases_txt($atts) {
+$yd_sp_premium_increases_txt = "Premium increases could apply for poor driving.";
+return $yd_sp_premium_increases_txt;
+}
+
+
+// ------------------------
+//
+// OBD STATS
+//
+// ------------------------
+function obd_help_number($atts) {
+$obd_help_number = "0333 2309 272";
+return $obd_help_number;
+}
+function obd_trakm8_email($atts) {
+$obd_trakm8_email = "telematics@trakm8.com";
+return $obd_trakm8_email;
+}
+
+// ------------------------
+//
+// INGENIE CONTACT STATS
+//
+// ------------------------
+function ig_main_number($atts) {
+$ig_main_number = "<span class='MAIN-TEL'>0330 678 0652</span>";
+return $ig_main_number;
+}
+
+
+// ------------------------
+//
+// Shortcodes
+// Will make editiable from wordpress admin when have time
+//
+// ------------------------
+
+add_shortcode('learner_get_a_quote_url', 'learner_get_a_quote_url');
+add_shortcode('learner_quotes_start_from_txt', 'learner_quotes_start_from_txt');
+add_shortcode('learner_low_excess_txt', 'learner_low_excess_txt');
+add_shortcode('learner_sp_dates_discount_txt', 'learner_sp_dates_discount_txt');
+
+add_shortcode('yd_up_front_txt', 'yd_up_front_txt');
+add_shortcode('yd_drive_safe_discount_txt', 'yd_drive_safe_discount_txt');
+add_shortcode('yd_renewal_discount_txt', 'yd_renewal_discount_txt');
+add_shortcode('yd_renewal_percentage_txt', 'yd_renewal_percentage_txt');
+add_shortcode('yd_frac_cus_discount_txt', 'yd_frac_cus_discount_txt');
+add_shortcode('yd_get_a_quote_url', 'yd_get_a_quote_url');
+add_shortcode('yd_sp_dates_discount_txt', 'yd_sp_dates_discount_txt');
+add_shortcode('yd_sp_dates_discount_txt_2', 'yd_sp_dates_discount_txt_2');
+add_shortcode('yd_sp_premium_increases_txt', 'yd_sp_premium_increases_txt');
+
+add_shortcode('obd_help_number', 'obd_help_number');
+add_shortcode('obd_trakm8_email', 'obd_trakm8_email');
+
+add_shortcode('ig_main_number', 'ig_main_number');
