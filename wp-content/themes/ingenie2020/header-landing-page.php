b6b51f3d439d84959d409b97a741a25880960998
Elton <elton@ingenie.com>
2021-07-15 14:38:38 +0100

    chore: landing header

---
+++ wp-content/themes/ingenie2020/header-landing-page.php
@@ -0,0 +1,31 @@
+<?php get_template_part('templates/partials/document-open'); ?>
+
+<header class="flex items-center justify-between bg-white shadow-md filter">
+
+    <div class="flex-1 px-4 py-2 m-2 text-left">
+
+        <!-- TrustBox widget - Micro Star -->
+        <div class="hidden md:block trustpilot-widget" data-locale="en-GB" data-template-id="5419b732fbfb950b10de65e5"
+            data-businessunit-id="4ed376ca000064000511ab4d" data-style-height="22px" data-style-width="100%"
+            data-theme="light">
+            <a href="https://uk.trustpilot.com/review/ingenie.com" target="_blank" rel="noopener">Trustpilot</a>
+        </div>
+        <!-- End TrustBox widget -->
+
+    </div>
+
+    <div class="flex-none px-4 py-2 m-2 align-middle">
+        <a href="/">
+            <!-- <span class="text-xl font-bold">ingenie</span> -->
+            <img src="/resources/img/ingenie-logo.svg" alt="ingenie Insurance" width="112" height="auto">
+        </a>
+    </div>
+
+    <div class="flex-1 px-4 py-2 m-2 text-right">
+
+    </div>
+
+</header>
+
+<? // <?php get_template_part('templates/partials/navbar', 'main'); 
+?>
